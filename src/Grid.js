3908f6686eb2edf5c780ed121e774474191820be
Ben Hernandez <benjammin234@gmail.com>
2020-08-15 15:29:44 -0700

    Static, unstyled grid

--- src/Grid.js
+++ src/Grid.js
@@ -1,6 +1,9 @@
 import React from "react";
+import Context from "./Context";
 
 export default class Grid extends React.Component {
+  static contextType = Context;
+
   // daily puzzle size = 15 x 15
   // sunday puzzle size = 21 x 21
   // black squares = blocks
@@ -15,25 +18,46 @@ export default class Grid extends React.Component {
       <div className="Grid">
         <table id="grid">
           <tr data-row="0">
-            <td data-col="0"></td>
-            <td data-col="1"></td>
-            <td data-col="2"></td>
-            <td data-col="3"></td>
+            <td data-col="0">
+              <div className="label">1</div>
+              <div className="fill"> </div>
+            </td>
+            <td data-col="1">
+              <div className="label">2</div>
+              <div className="fill"> </div>
+            </td>
+            <td data-col="2">
+              <div className="label">3</div>
+              <div className="fill"> </div>
+            </td>
+            <td data-col="3">
+              <div className="label">4</div>
+              <div className="fill"> </div>
+            </td>
           </tr>
           <tr data-row="1">
-            <td data-col="0"></td>
+            <td data-col="0">
+              <div className="label">5</div>
+              <div className="fill"> </div>
+            </td>
             <td data-col="1"></td>
             <td data-col="2"></td>
             <td data-col="3"></td>
           </tr>
           <tr data-row="2">
-            <td data-col="0"></td>
+            <td data-col="0">
+              <div className="label">6</div>
+              <div className="fill"> </div>
+            </td>
             <td data-col="1"></td>
             <td data-col="2"></td>
             <td data-col="3"></td>
           </tr>
           <tr data-row="3">
-            <td data-col="0"></td>
+            <td data-col="0">
+              <div className="label">7</div>
+              <div className="fill"> </div>
+            </td>
             <td data-col="1"></td>
             <td data-col="2"></td>
             <td data-col="3"></td>
