904eef0d43e54de4f8457233219f31ec991daa56
Ben Hernandez <benjammin234@gmail.com>
2020-09-18 14:35:29 -0700

    Ready to merge

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PuzzleEditor from "./PuzzleEditor";
+// import PuzzleEditor from "./PuzzleEditor";
 import Context from "./Context";
 
 export default class Home extends React.Component {
